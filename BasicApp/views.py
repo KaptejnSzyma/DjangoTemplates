face33405f1c87ac85d8947229ef7b7180116ee7
Szymek <szymon.rzadca@gmail.com>
2018-09-12 17:00:27 +0200

    added the context dictionary to the return statement

--- BasicApp/views.py
+++ BasicApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     context_dict = {'text': 'hello_world', 'number': 100}
-    return render(request, 'BasicApp/index.html')
+    return render(request, 'BasicApp/index.html', context_dict)
 
 
 def other(request):
